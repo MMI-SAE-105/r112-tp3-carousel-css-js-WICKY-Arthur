9260f03ff396f60523608b3530097586f437fe8d
WICKY-Arthur <arthur.wicky@edu.univ-fcomte.fr>
2024-11-19 16:46:16 +0100

    défilement en fonction de la largeur d'une image

--- src/js/script.js
+++ src/js/script.js
@@ -3,19 +3,18 @@
 const carousel = document.querySelector(".carousel__container");
 const prevButton = document.querySelector(".carousel__button--prev");
 const nextButton = document.querySelector(".carousel__button--next");
-console.log(carousel);
-console.log(prevButton);
-console.log(nextButton);
+const premierItem = document.querySelector(".carousel__item");
+const scrollAmount = premierItem.clientWidth;
 
 // Largeur de défilement d’un item
 if (carousel) {
     // Scroll au clic sur le bouton précédent
     prevButton.addEventListener("click", () => {
-        carousel.scrollBy({ left: -250, behavior: 'smooth' });
+        carousel.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
     });
 
     // Scroll au clic sur le bouton suivant
     nextButton.addEventListener("click", () => {
-        carousel.scrollBy({ left: 250, behavior: 'smooth' });
+        carousel.scrollBy({ left: scrollAmount, behavior: 'smooth' });
     });
 }
